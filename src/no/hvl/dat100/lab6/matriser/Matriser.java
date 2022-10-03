805d6731a3e3989eac727c9e72d52eb31c3352e1
Erlend Nesheim <54097889+hvl577705@users.noreply.github.com>
2022-10-03 02:01:34 +0200

    Update Matriser.java
    Oppdaterte alle obligatoriske oppgaver

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -4,47 +4,77 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
+		for(int[] i:matrise) {
+			for(int y:i) {
+				System.out.print(y + " ");
+			}
+			System.out.println();
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
-		
+		String a = "";
+		for(int[] i:matrise) {
+			for(int y:i) {
+				a = a + y + " ";
+			}
+			a = a + "\n";
+		}
+		return a;
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("skaler ikke implementert");
-	
+		int[][] newTab = new int[matrise.length][matrise[0].length];
+		for(int i = 0; i<matrise.length; i++) {
+			for(int y = 0; y<matrise[i].length; y++) {
+				newTab[i][y] = matrise[i][y] * tall;
+			}
+		}
+		return newTab;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("erLik ikke implementert");
+		boolean lik = true;
+		for(int i = 0; i<a.length; i++) {
+			for(int y = 0; y<a.length; y++) {
+				if(a[i][y] != b[i][y]) {
+					lik = false;
+				}
+			}
+		}
+		return lik;
 	}
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
-
-		// TODO
-		throw new UnsupportedOperationException("speile ikke implementert");
-	
+		int[][] nyTab = new int[matrise.length][matrise[0].length]; 
+		for(int i = 0; i<matrise.length; i++) {
+			for(int y = 0; y<matrise[0].length; y++) {
+				nyTab[i][y] = matrise[y][i];
+			}
+		}
+		return nyTab;
+		//ikke ferdig
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("multipliser ikke implementert");
-	
+		int temp = 0;
+		int[][] tabC = new int[a.length][b[0].length]; 
+		for(int i = 0; i<a.length; i++) {
+			for(int y = 0; y<b.length; y++) {
+				for(int x = 0; x<b[0].length; x++) {
+					for(int z = 0; z<a.length; z++) {
+						temp = temp + b[y][z]*a[z][y];
+					}
+				}
+				tabC[i][y] = temp;
+			}
+			
+		}
+		return tabC;
 	}
 }
